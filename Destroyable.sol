d4b92b908f4ed7a74e9e3ec4106d9d11317d5594
George Doujaiji <gdoujaiji@gmail.com>
2020-08-02 19:20:41 +0300

    Updated Destroyable.sol

--- Destroyable.sol
+++ Destroyable.sol
@@ -1,6 +1,7 @@
 import "./Ownable.sol";
 pragma solidity 0.5.12;
 
+// Destroyable contract
 contract Destroyable is Ownable{
     function destroy() public onlyOwner{
         address payable receiver = msd.sender;

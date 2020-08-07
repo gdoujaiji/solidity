c63094cc0b247c3bbc2b9d268ac9e6089cf5b18b
George Doujaiji <gdoujaiji@gmail.com>
2020-08-08 00:12:31 +0300

    Created Token.sol

---
+++ Token.sol
@@ -0,0 +1,63 @@
+pragma solidity 0.5.12;
+import "./Ownable.sol";
+
+contract ERC20 {
+    using SafeMath for uint256;
+    using Address for address;
+    
+    string private _name;
+    string private _symbol;
+    uint8 private _decimals;
+    uint256 private _totalSupply;
+    
+    mapping(address => uint256) private _balances; // holds the total balance of the token that the contract have
+    
+    event Transfer(
+        address indexed _from,
+        address indexed _to,
+        uint256 _value
+    );
+
+    // to initialise the contract
+    constructor(string memory name, string memory symbol, uint8 decimals) public {
+        _name = name;
+        _symbol = symbol;
+        _decimals = decimals;
+        
+    } 
+    
+    function name() public view returns (string memory){
+        return _name;
+    }
+    
+    function symbol() public view returns (string memory){
+        return _symbol;
+    }
+    
+    function decimals() public view returns (uint8){
+        return _decimals;
+    }
+    
+    function totalSupply() public view override returns (uint256){
+        return _totalSupply;
+    }
+    
+    function balanceOf(address account) public view override returns(uint256){
+        return _balances[account];
+    }
+    
+    function mint(address account, uint256 amount) public {
+        require(account != address(0), "ERC20: mint to the zero address");
+
+        _beforeTokenTransfer(address(0), account, amount);
+
+        _totalSupply = _totalSupply.add(amount);
+        _balances[account] = _balances[account].add(amount);
+        emit Transfer(address(0), account, amount);
+    }
+
+    function transfer(address _to, uint256 _value) public override returns (bool success){
+        _transfer(_msgSender(), recipient, amount);
+        return true;
+    }
+}
